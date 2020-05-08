fe9a172638eb2816952459621e891219706739e3
Mauricio Homma <mhhomma@gmail.com>
2020-05-08 15:03:52 -0300

    fix base app.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,7 @@ import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={MovieList} />
-          <Route path="/movies/new" component={NewMovie} />
-          <Route exact path="/movies/:id" component={MovieDetails} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
+      <div>Movie Card Library CRUD</div>
   );
 }
 
